a856a7d2dbb99881cb3ec2297b69753536441651
azerroth11 <mallory.berthome@gmail.com>
2023-03-12 14:47:19 +0100

    *

--- script.js
+++ script.js
@@ -1,121 +1,266 @@
+// const data = [
+//   [{ name: 'affiche processus', format: '1/2' }],
+//   [{ name: 'divingWoman', format: '1/2' }],
+//   [{ name: 'isolated posters', format: '1/2' }],
+//   [{ name: 'couverture mémoire', format: '1/2' }],
+//   [{ name: 'bonhomme geometrique', format: '1/2' }],
+//   [{ name: 'PFCP', format: '1/2' }],
+//   [{ name: 'la danse des oiseaux', format: '1/2' }],
+//   [{ name: 'maulevrier', format: '1/2' }],
+//   [{ name: 'visage noir et blanc', format: '1/2' }],
+//   [{ name: 'olio e burro', format: '1/2' }],
+//   [{ name: 'panonica', format: '1/2' }],
+//   [{ name: 'fanzine', format: '1/2' }],
+//   [{ name: 'savon 1', format: '1/2' }],
+//   [{ name: 'oeil', format: '1/2' }],
+//   [{ name: 'vélo sport', format: '2/1' }],
+//   [{ name: 'workshop typo', format: '2/1' }],
+//   [{ name: 'bal des pompiers', format: '2/1' }],
+// ]
+
 const data = [
-  [{ name: 'affiche processus', format: '1/2' }],
-  [{ name: 'divingWoman', format: '1/2' }],
-  [{ name: 'isolated posters', format: '1/2' }],
-  [{ name: 'couverture mémoire', format: '1/2' }],
-  [{ name: 'bonhomme geometrique', format: '1/2' }],
-  [{ name: 'PFCP', format: '1/2' }],
-  [{ name: 'la danse des oiseaux', format: '1/2' }],
-  [{ name: 'maulevrier', format: '1/2' }],
-  [{ name: 'visage noir et blanc', format: '1/2' }],
-  [{ name: 'olio e burro', format: '1/2' }],
-  [{ name: 'panonica', format: '1/2' }],
-  [{ name: 'fanzine', format: '1/2' }],
-  [{ name: 'savon 1', format: '1/2' }],
-  [{ name: 'oeil', format: '1/2' }],
-  [{ name: 'vélo sport', format: '2/1' }],
-  [{ name: 'workshop typo', format: '2/1' }],
-  [{ name: 'bal des pompiers', format: '2/1' }],
+  {
+    name: 'Olio e Burro',
+    cover: './img/gallery/olio e burro.webp',
+    keywords: ['Branding', 'Illustration'],
+    challenge:
+      "Trouver l'équilibre entre l'aspect artisanal et moderne, tout en transmettant la simplicité et le savoir-faire de leur cuisine avec une identité visuelle élégante et contemporaine pour attirer les clients.",
+    solution:
+      "Une palette de couleurs naturelles, des illustrations dessinées à la main et des typographies simples mais modernes pour créer une identité visuelle qui reflète parfaitement l'atmosphère chaleureuse et authentique d'Olio e Burro.",
+    imgs: [
+      './img/Projet 1/olio 1.webp',
+      './img/Projet 1/olio 6.webp',
+      './img/Projet 1/olio 7.webp',
+      './img/Projet 1/olio 5.webp',
+      './img/Projet 1/olio 4.webp',
+      './img/Projet 1/olio 3.webp',
+      './img/Projet 1/olio 2.webp',
+    ],
+    description:
+      "Olio e Burro est un restaurant né de la rencontre d'un français et d'une italienne amoureux de leur culture. Ils proposent une cuisine locale et sans gluten qui valorise la simplicité et le savoir faire. Le couple propose des plats fait maison à partir de produits bruts.  Cette identité est une vitrine de leur singularité : elle traduit un artisanat qui se veut simple mais chaleureux. La conception de cette identité est le fruit de recherches graphiques et plastiques prolifiques autour de la cuisine.",
+  },
+  {
+    name: 'Olio e Burro',
+    cover: './img/gallery/olio e burro.webp',
+    keywords: ['Branding', 'Illustration'],
+    challenge:
+      "Trouver l'équilibre entre l'aspect artisanal et moderne, tout en transmettant la simplicité et le savoir-faire de leur cuisine avec une identité visuelle élégante et contemporaine pour attirer les clients.",
+    solution:
+      "Une palette de couleurs naturelles, des illustrations dessinées à la main et des typographies simples mais modernes pour créer une identité visuelle qui reflète parfaitement l'atmosphère chaleureuse et authentique d'Olio e Burro.",
+    imgs: [
+      './img/Projet 1/olio 1.webp',
+      './img/Projet 1/olio 6.webp',
+      './img/Projet 1/olio 7.webp',
+      './img/Projet 1/olio 5.webp',
+      './img/Projet 1/olio 4.webp',
+      './img/Projet 1/olio 3.webp',
+      './img/Projet 1/olio 2.webp',
+    ],
+    description:
+      "Olio e Burro est un restaurant né de la rencontre d'un français et d'une italienne amoureux de leur culture. Ils proposent une cuisine locale et sans gluten qui valorise la simplicité et le savoir faire. Le couple propose des plats fait maison à partir de produits bruts.  Cette identité est une vitrine de leur singularité : elle traduit un artisanat qui se veut simple mais chaleureux. La conception de cette identité est le fruit de recherches graphiques et plastiques prolifiques autour de la cuisine.",
+  },
+  {
+    name: 'Olio e Burro',
+    cover: './img/gallery/olio e burro.webp',
+    keywords: ['Branding', 'Illustration'],
+    challenge:
+      "Trouver l'équilibre entre l'aspect artisanal et moderne, tout en transmettant la simplicité et le savoir-faire de leur cuisine avec une identité visuelle élégante et contemporaine pour attirer les clients.",
+    solution:
+      "Une palette de couleurs naturelles, des illustrations dessinées à la main et des typographies simples mais modernes pour créer une identité visuelle qui reflète parfaitement l'atmosphère chaleureuse et authentique d'Olio e Burro.",
+    imgs: [
+      './img/Projet 1/olio 1.webp',
+      './img/Projet 1/olio 6.webp',
+      './img/Projet 1/olio 7.webp',
+      './img/Projet 1/olio 5.webp',
+      './img/Projet 1/olio 4.webp',
+      './img/Projet 1/olio 3.webp',
+      './img/Projet 1/olio 2.webp',
+    ],
+    description:
+      "Olio e Burro est un restaurant né de la rencontre d'un français et d'une italienne amoureux de leur culture. Ils proposent une cuisine locale et sans gluten qui valorise la simplicité et le savoir faire. Le couple propose des plats fait maison à partir de produits bruts.  Cette identité est une vitrine de leur singularité : elle traduit un artisanat qui se veut simple mais chaleureux. La conception de cette identité est le fruit de recherches graphiques et plastiques prolifiques autour de la cuisine.",
+  },
+  {
+    name: 'Olio e Burro',
+    cover: './img/gallery/olio e burro.webp',
+    keywords: ['Branding', 'Illustration'],
+    challenge:
+      "Trouver l'équilibre entre l'aspect artisanal et moderne, tout en transmettant la simplicité et le savoir-faire de leur cuisine avec une identité visuelle élégante et contemporaine pour attirer les clients.",
+    solution:
+      "Une palette de couleurs naturelles, des illustrations dessinées à la main et des typographies simples mais modernes pour créer une identité visuelle qui reflète parfaitement l'atmosphère chaleureuse et authentique d'Olio e Burro.",
+    imgs: [
+      './img/Projet 1/olio 1.webp',
+      './img/Projet 1/olio 6.webp',
+      './img/Projet 1/olio 7.webp',
+      './img/Projet 1/olio 5.webp',
+      './img/Projet 1/olio 4.webp',
+      './img/Projet 1/olio 3.webp',
+      './img/Projet 1/olio 2.webp',
+    ],
+    description:
+      "Olio e Burro est un restaurant né de la rencontre d'un français et d'une italienne amoureux de leur culture. Ils proposent une cuisine locale et sans gluten qui valorise la simplicité et le savoir faire. Le couple propose des plats fait maison à partir de produits bruts.  Cette identité est une vitrine de leur singularité : elle traduit un artisanat qui se veut simple mais chaleureux. La conception de cette identité est le fruit de recherches graphiques et plastiques prolifiques autour de la cuisine.",
+  },
+  {
+    name: 'Olio e Burro',
+    cover: './img/gallery/olio e burro.webp',
+    keywords: ['Branding', 'Illustration'],
+    challenge:
+      "Trouver l'équilibre entre l'aspect artisanal et moderne, tout en transmettant la simplicité et le savoir-faire de leur cuisine avec une identité visuelle élégante et contemporaine pour attirer les clients.",
+    solution:
+      "Une palette de couleurs naturelles, des illustrations dessinées à la main et des typographies simples mais modernes pour créer une identité visuelle qui reflète parfaitement l'atmosphère chaleureuse et authentique d'Olio e Burro.",
+    imgs: [
+      './img/Projet 1/olio 1.webp',
+      './img/Projet 1/olio 6.webp',
+      './img/Projet 1/olio 7.webp',
+      './img/Projet 1/olio 5.webp',
+      './img/Projet 1/olio 4.webp',
+      './img/Projet 1/olio 3.webp',
+      './img/Projet 1/olio 2.webp',
+    ],
+    description:
+      "Olio e Burro est un restaurant né de la rencontre d'un français et d'une italienne amoureux de leur culture. Ils proposent une cuisine locale et sans gluten qui valorise la simplicité et le savoir faire. Le couple propose des plats fait maison à partir de produits bruts.  Cette identité est une vitrine de leur singularité : elle traduit un artisanat qui se veut simple mais chaleureux. La conception de cette identité est le fruit de recherches graphiques et plastiques prolifiques autour de la cuisine.",
+  },
 ]
 
-const links = Array.from(document.querySelectorAll('.link'))
-const header = document.querySelector('header')
+function buildProjectSection(main, section, project) {
+  createProjectName()
 
-links.forEach((link) => {
-  link.addEventListener('click', (e) => {
-    toggleElement('main', 'header')
-    const headerLinks = ['PROJETS', 'À PROPOS', 'CONTACT']
-    setTimeout(() => {
-      header.firstElementChild.remove()
-      headerLinks.forEach((headerLink) => {
-        const h2 = document.querySelector('header').appendChild(document.createElement('h2'))
-        h2.innerText = headerLink
-        if (
-          headerLink == e.target.innerText ||
-          (header.children[2] && e.target.innerText == 'Contactez-moi!')
-        ) {
-          h2.classList.add('selectedLink')
-        }
-        h2.addEventListener('click', () => {
-          if (!h2.classList.contains('selectedLink')) {
-            setLinkColor(h2)
-            populateMain()
-          } else if (h2.innerText === 'À PROPOS') {
-          }
-        })
+  const cover = section.appendChild(document.createElement('img'))
+  cover.classList.add('cover')
+  cover.src = `${project.cover}`
+
+  const keywords = section.appendChild(document.createElement('div'))
+  keywords.classList.add('keywords')
+  project.keywords.forEach((keyword) => {
+    const h3 = keywords.appendChild(document.createElement('h3'))
+    h3.textContent = keyword
+  })
+
+  const description = section.appendChild(document.createElement('div'))
+  description.classList.add('description')
+
+  description.appendChild(document.createElement('h4')).textContent = 'Le Challenge'
+  description.appendChild(document.createElement('p')).textContent = `${project.challenge}`
+  description.appendChild(document.createElement('h4')).textContent = 'Ma Solution'
+  description.appendChild(document.createElement('p')).textContent = `${project.solution}`
+
+  const button = section.appendChild(document.createElement('a'))
+  button.href = '#'
+  button.classList.add('discover')
+  button.textContent = 'DÉCOUVRIR'
+
+  button.addEventListener('click', () => {
+    main.classList.toggle('hidden')
+    setTimeout(function () {
+      clearContent(section)
+      createProjectName()
+      createSwiperDiv(section)
+      populateSwiperWrapper(project)
+      initSwiper()
+
+      const projetDesc = section.appendChild(document.createElement('div'))
+      projetDesc.classList.add('description')
+
+      projetDesc.appendChild(document.createElement('h4')).textContent = 'Le Client'
+
+      const projetDescP = projetDesc.appendChild(document.createElement('p'))
+      projetDescP.textContent = `${project.description}`
+
+      const retour = section.appendChild(document.createElement('a'))
+      retour.textContent = 'RETOUR'
+      retour.addEventListener('click', () => {
+        main.classList.toggle('hidden')
+        setTimeout(function () {
+          clearContent(section)
+          buildProjectSection(main, section, project)
+          setTimeout(() => main.classList.toggle('hidden'), 250)
+        }, 500)
       })
-      populateMain()
-      toggleElement('main', 'header')
+
+      setTimeout(() => main.classList.toggle('hidden'), 250)
     }, 500)
   })
-})
 
-function toggleElement(...elements) {
-  elements.forEach((element) => {
-    document.querySelector(element).classList.toggle('hidden')
-  })
-}
+  function createProjectName() {
+    const infos = section.appendChild(document.createElement('div'))
+    infos.classList.add('infos')
 
-function clearMainContent(main) {
-  while (main.firstElementChild) {
-    main.firstElementChild.remove()
+    const infosH1 = infos.appendChild(document.createElement('h1'))
+    infosH1.textContent = `0${data.indexOf(project) + 1}`
+
+    const infosH2 = infos.appendChild(document.createElement('h2'))
+    infosH2.textContent = `${project.name}`
   }
 }
 
-function setLinkColor(h2) {
-  Array.from(header.children).forEach((link) => {
-    link.classList.remove('selectedLink')
-  })
-  h2.classList.add('selectedLink')
-}
-
-function populateMain() {
+function projectsBuilding() {
   const main = document.querySelector('main')
-  clearMainContent(main)
-  if (document.querySelector('.selectedLink').innerText == 'PROJETS')
-    data.forEach((folder) => {
-      const img = document.createElement('img')
-      img.src = `./img/gallery/${folder[0].name}.webp`
-      main.appendChild(img)
-      if (folder[0].format == '2/1') {
-        img.classList.add('horizontal')
-      }
-    })
-  else if (document.querySelector('.selectedLink').innerText == 'À PROPOS') {
-    const aboutDiv1 = main.appendChild(document.createElement('div'))
-    aboutDiv1.classList.add('aboutDiv')
-    const aboutDiv1H1 = aboutDiv1.appendChild(document.createElement('h1'))
-    aboutDiv1H1.innerText = 'À propos'
-    const aboutDiv1P = aboutDiv1.appendChild(document.createElement('p'))
-    aboutDiv1P.innerText =
-      "Je m'appelle Isaure, je suis étudiante en graphisme. Curieuse et polyvalente, je vous invite à découvrir mon travail. Ma pratique se veut exploratoire et pluri-disciplinaire."
-    const aboutDiv2 = main.appendChild(document.createElement('div'))
-    aboutDiv2.classList.add('experiencesDiv')
-    const aboutDiv2H1 = aboutDiv2.appendChild(document.createElement('h1'))
-    aboutDiv2H1.innerText = 'Expériences professionelles'
-    const aboutDiv2Ul = aboutDiv2.appendChild(document.createElement('ul'))
-    const experiencesLi1 = aboutDiv2Ul.appendChild(document.createElement('li'))
-    experiencesLi1.innerText = "Stage à l'atelier graphique KOLK"
-    const experiencesLi2 = aboutDiv2Ul.appendChild(document.createElement('li'))
-    experiencesLi2.innerText = "Stage dans l'agence web Akagi"
-    const experiencesLi3 = aboutDiv2Ul.appendChild(document.createElement('li'))
-    experiencesLi3.innerText = 'Stage au sein du pôle signalétique de Nantes Métropole'
-    const aboutDiv3 = main.appendChild(document.createElement('div'))
-    aboutDiv3.classList.add('formationDiv')
-    const aboutDiv3H1 = aboutDiv3.appendChild(document.createElement('h1'))
-    aboutDiv3H1.innerText = 'Formation'
-    const formation1 = aboutDiv3.appendChild(document.createElement('p'))
-    formation1.innerText = 'DNMADe Graphisme - Option identité multi-support'
-  } else {
-    const contactIMG = main.appendChild(document.createElement('img'))
-    contactIMG.classList.add('contactIMG')
-    contactIMG.src = './img/gallery/visage noir et blanc.webp'
-    const contactDiv = main.appendChild(document.createElement('div'))
-    contactDiv.classList.add('contactDiv')
-    const contactH2 = contactDiv.appendChild(document.createElement('h2'))
-    contactH2.innerText = 'ISAURE BERTHOMÉ'
-    const contactMail = contactDiv.appendChild(document.createElement('a'))
-    contactMail.href = 'mailto:isaure.ber@gmail.com'
-    contactMail.innerText = 'CONTACTEZ-MOI !'
+  data.forEach((project) => {
+    const section = main.appendChild(document.createElement('div'))
+    section.classList.add(`project${data.indexOf(project) + 1}`, 'section')
+    buildProjectSection(main, section, project)
+  })
+}
+projectsBuilding()
+
+function clearContent(section) {
+  while (section.firstChild) {
+    section.removeChild(section.lastChild)
   }
+}
+
+function createSwiperDiv(section) {
+  const swiperDiv = section.appendChild(document.createElement('div'))
+  swiperDiv.classList.add('swiper')
+
+  const swiperWrapper = swiperDiv.appendChild(document.createElement('div'))
+  swiperWrapper.classList.add('swiper-wrapper')
+
+  const swiperNavPrev = swiperDiv.appendChild(document.createElement('div'))
+  swiperNavPrev.classList.add('swiper-button-prev')
+
+  const swiperNavNext = swiperDiv.appendChild(document.createElement('div'))
+  swiperNavNext.classList.add('swiper-button-next')
+
+  const swiperPagination = swiperDiv.appendChild(document.createElement('div'))
+  swiperPagination.classList.add('swiper-pagination')
+
+  return swiperDiv
+}
+
+function populateSwiperWrapper(project) {
+  const entries = Array.from(project.imgs)
+
+  entries.forEach((entry) => {
+    const swiperSlide = document
+      .querySelector('.swiper-wrapper')
+      .appendChild(document.createElement('div'))
+    swiperSlide.classList.add('swiper-slide')
+
+    const swiperIMG = swiperSlide.appendChild(document.createElement('img'))
+    swiperIMG.src = `${entry}`
+  })
+}
+
+function initSwiper() {
+  return new Swiper('.swiper', {
+    slidesPerView: 'auto',
+    centeredSlides: true,
+    // spaceBetween: 30,
+    grabCursor: true,
+    freeMode: {
+      enabled: true,
+      sticky: true,
+    },
+    keyboard: {
+      enabled: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  })
 }
