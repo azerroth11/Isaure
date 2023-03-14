40080eb79ee8968882bb7da65e12cb3891211bc1
azerroth11 <mallory.berthome@gmail.com>
2023-03-14 15:33:06 +0100

    *

--- script.js
+++ script.js
@@ -32,9 +32,9 @@ const data = [
       './img/Projet 1/olio 6.webp',
       './img/Projet 1/olio 7.webp',
       './img/Projet 1/olio 5.webp',
-      './img/Projet 1/olio 4.webp',
-      './img/Projet 1/olio 3.webp',
-      './img/Projet 1/olio 2.webp',
+      // './img/Projet 1/olio 4.webp',
+      // './img/Projet 1/olio 3.webp',
+      // './img/Projet 1/olio 2.webp',
     ],
     description:
       "Olio e Burro est un restaurant né de la rencontre d'un français et d'une italienne amoureux de leur culture. Ils proposent une cuisine locale et sans gluten qui valorise la simplicité et le savoir faire. Le couple propose des plats fait maison à partir de produits bruts. C'est un endroit idéal pour les amateurs de cuisine italienne à la recherche d'ingrédients authentiques. ",
@@ -64,16 +64,16 @@ const data = [
     solution:
       "Un flyer inspiré des cartes de métro, dépliable pour fournir un plan clair et complet du parc, ainsi que des informations pratiques et historiques sur le lieu. L'identité visuelle a été conçue pour refléter la diversité de la végétation, permettant aux visiteurs de se repérer facilement dans le parc et d'apprécier sa beauté tout en ayant accès à toutes les informations nécessaires.",
     imgs: [
-      './img/Projet 3/Maulévrier 1.webp',
+      // './img/Projet 3/Maulévrier 1.webp',
       './img/Projet 3/Maulévrier 8.webp',
       './img/Projet 3/Maulévrier 2.webp',
-      './img/Projet 3/Maulévrier 3.webp',
+      './img/Projet 3/Maulévrier 9.webp',
+      './img/Projet 3/Maulévrier 10.webp',
+      // './img/Projet 3/Maulévrier 3.webp',
       './img/Projet 3/Maulévrier 4.webp',
       './img/Projet 3/Maulévrier 5.webp',
       './img/Projet 3/Maulévrier 6.webp',
       './img/Projet 3/Maulévrier 7.webp',
-      './img/Projet 3/Maulévrier 9.webp',
-      './img/Projet 3/Maulévrier 10.webp',
     ],
     description:
       "Le Parc Oriental de Maulévrier est un jardin botanique japonais créé en 1899 par Alexander Marcel, un industriel passionné de botanique et d'arts asiatiques. S'étendant sur plus de 29 hectares, il contient plus de 300 espèces végétales venues du Japon, de Chine et de Corée, ainsi que de nombreux éléments traditionnels de l'art de vivre japonais tels que des ponts, des lanternes, des pagodes et des pavillons.",
@@ -89,99 +89,32 @@ const data = [
     imgs: [
       './img/Projet 4/Racecars 1.webp',
       './img/Projet 4/Racecars 2.webp',
-      './img/Projet 4/Racecars 3.webp',
+      // './img/Projet 4/Racecars 3.webp',
       './img/Projet 4/Racecars 4.webp',
     ],
     description:
       "RaceCars Location est une entreprise de location de voitures de sport et de prestige située à Nantes, en France. Elle propose une flotte de véhicules haut de gamme, tels que des Ferrari, Lamborghini et Porsche, pour des événements spéciaux ou des sorties de loisirs. C'est un choix idéal pour les amateurs de voitures de luxe cherchant une expérience de conduite unique.",
   },
   {
-    name: 'Olio e Burro',
-    cover: './img/gallery/olio e burro.webp',
-    keywords: ['Branding', 'Illustration'],
-    challenge:
-      "Trouver l'équilibre entre l'aspect artisanal et moderne, tout en transmettant la simplicité et le savoir-faire de leur cuisine avec une identité visuelle élégante et contemporaine pour attirer les clients.",
-    solution:
-      "Une palette de couleurs naturelles, des illustrations dessinées à la main et des typographies simples mais modernes pour créer une identité visuelle qui reflète parfaitement l'atmosphère chaleureuse et authentique d'Olio e Burro.",
+    name: 'Inktober',
+    cover: './img/Projet 5/Inktober 1.webp',
+    keywords: ['Illustration'],
+    challenge: '',
+    solution: '',
     imgs: [
-      './img/Projet 1/olio 1.webp',
-      './img/Projet 1/olio 6.webp',
-      './img/Projet 1/olio 7.webp',
-      './img/Projet 1/olio 5.webp',
-      './img/Projet 1/olio 4.webp',
-      './img/Projet 1/olio 3.webp',
-      './img/Projet 1/olio 2.webp',
+      './img/Projet 5/Inktober 2.webp',
+      './img/Projet 5/Inktober 1.webp',
+      './img/Projet 5/Inktober 4.webp',
+      './img/Projet 5/Inktober 3.webp',
+      // './img/Projet 5/Inktober 5.webp',
+      // './img/Projet 5/Inktober 6.webp',
+      './img/Projet 5/Inktober 7.webp',
     ],
-    description:
-      "Olio e Burro est un restaurant né de la rencontre d'un français et d'une italienne amoureux de leur culture. Ils proposent une cuisine locale et sans gluten qui valorise la simplicité et le savoir faire. Le couple propose des plats fait maison à partir de produits bruts.  Cette identité est une vitrine de leur singularité : elle traduit un artisanat qui se veut simple mais chaleureux. La conception de cette identité est le fruit de recherches graphiques et plastiques prolifiques autour de la cuisine.",
+    description: 'Réalisation de dessins à l’encre à l’occasion du challenge Inktober 2021.',
   },
 ]
 
 function buildProjectSection(main, section, project) {
-  createProjectName()
-
-  const cover = section.appendChild(document.createElement('img'))
-  cover.classList.add('cover')
-  cover.src = `${project.cover}`
-
-  const keywords = section.appendChild(document.createElement('div'))
-  keywords.classList.add('keywords')
-  project.keywords.forEach((keyword) => {
-    const h3 = keywords.appendChild(document.createElement('h3'))
-    h3.textContent = keyword
-  })
-
-  const description = section.appendChild(document.createElement('div'))
-  description.classList.add('description')
-
-  description.appendChild(document.createElement('h4')).textContent = 'Le Client'
-  const projetDescP = description.appendChild(document.createElement('p'))
-  projetDescP.textContent = `${project.description}`
-
-  const button = section.appendChild(document.createElement('a'))
-  button.classList.add('discover')
-  button.textContent = 'DÉCOUVRIR'
-
-  button.addEventListener('click', () => {
-    main.classList.toggle('hidden')
-    setTimeout(function () {
-      clearContent(section)
-      createProjectName()
-      createSwiperDiv(section)
-      populateSwiperWrapper(project)
-      initSwiper()
-
-      const projetDesc = section.appendChild(document.createElement('div'))
-      projetDesc.classList.add('description')
-      projetDesc.appendChild(document.createElement('h4')).textContent = 'Le Challenge'
-      projetDesc.appendChild(document.createElement('p')).textContent = `${project.challenge}`
-      projetDesc.appendChild(document.createElement('h4')).textContent = 'Ma Solution'
-      projetDesc.appendChild(document.createElement('p')).textContent = `${project.solution}`
-
-      const retour = section.appendChild(document.createElement('a'))
-      retour.textContent = 'RETOUR'
-      retour.addEventListener('click', () => {
-        main.classList.toggle('hidden')
-        setTimeout(function () {
-          clearContent(section)
-          buildProjectSection(main, section, project)
-          setTimeout(() => main.classList.toggle('hidden'), 250)
-        }, 500)
-      })
-
-      if (window.innerWidth >= 1024) {
-        section.lastChild.insertAdjacentElement(
-          'afterend',
-          document.getElementsByClassName('swiper')[0]
-        )
-      } else {
-        console.log('Screen probably too small')
-      }
-
-      setTimeout(() => main.classList.toggle('hidden'), 250)
-    }, 500)
-  })
-
   function createProjectName() {
     const infos = section.appendChild(document.createElement('div'))
     infos.classList.add('infos')
@@ -192,9 +125,97 @@ function buildProjectSection(main, section, project) {
     const infosH2 = infos.appendChild(document.createElement('h2'))
     infosH2.textContent = `${project.name}`
   }
+  createProjectName()
 
-  if (window.innerWidth >= 1024) {
-    section.lastChild.insertAdjacentElement('afterend', cover)
+  if (window.innerWidth <= 768) {
+    const cover = section.appendChild(document.createElement('img'))
+    cover.classList.add('cover')
+    cover.src = `${project.cover}`
+
+    const keywords = section.appendChild(document.createElement('div'))
+    keywords.classList.add('keywords')
+    project.keywords.forEach((keyword) => {
+      const h3 = keywords.appendChild(document.createElement('h3'))
+      h3.textContent = keyword
+    })
+
+    const description = section.appendChild(document.createElement('div'))
+    description.classList.add('description')
+
+    description.appendChild(document.createElement('h4')).textContent = 'Le Client'
+    const projetDescP = description.appendChild(document.createElement('p'))
+    projetDescP.textContent = `${project.description}`
+
+    const button = section.appendChild(document.createElement('a'))
+    button.classList.add('discover')
+    button.textContent = 'DÉCOUVRIR'
+
+    button.addEventListener('click', () => {
+      main.classList.toggle('hidden')
+      setTimeout(function () {
+        clearContent(section)
+        createProjectName()
+        createSwiperDiv(section)
+        populateSwiperWrapper(project)
+        initSwiper()
+
+        const projetDesc = section.appendChild(document.createElement('div'))
+        projetDesc.classList.add('description')
+        projetDesc.appendChild(document.createElement('h4')).textContent = 'Le Challenge'
+        projetDesc.appendChild(document.createElement('p')).textContent = `${project.challenge}`
+        projetDesc.appendChild(document.createElement('h4')).textContent = 'Ma Solution'
+        projetDesc.appendChild(document.createElement('p')).textContent = `${project.solution}`
+
+        const retour = section.appendChild(document.createElement('a'))
+        retour.textContent = 'RETOUR'
+        retour.addEventListener('click', () => {
+          main.classList.toggle('hidden')
+          setTimeout(function () {
+            clearContent(section)
+            buildProjectSection(main, section, project)
+            setTimeout(() => main.classList.toggle('hidden'), 250)
+          }, 500)
+        })
+
+        if (window.innerWidth >= 1024) {
+          section.lastChild.insertAdjacentElement(
+            'afterend',
+            document.getElementsByClassName('swiper')[0]
+          )
+        } else {
+          console.log('Screen probably too small')
+        }
+
+        setTimeout(() => main.classList.toggle('hidden'), 250)
+      }, 500)
+    })
+
+    if (window.innerWidth >= 1024) {
+      section.lastChild.insertAdjacentElement('afterend', cover)
+    }
+  } else {
+    const keywords = section.appendChild(document.createElement('div'))
+    keywords.classList.add('keywords')
+    project.keywords.forEach((keyword) => {
+      const h3 = keywords.appendChild(document.createElement('h3'))
+      h3.textContent = keyword
+    })
+
+    const description = section.appendChild(document.createElement('div'))
+    description.classList.add('description')
+    description.appendChild(document.createElement('h4')).textContent = 'Le Client'
+    const projetDescP = description.appendChild(document.createElement('p'))
+    projetDescP.textContent = `${project.description}`
+    description.appendChild(document.createElement('h4')).textContent = 'Le Challenge'
+    description.appendChild(document.createElement('p')).textContent = `${project.challenge}`
+    description.appendChild(document.createElement('h4')).textContent = 'Ma Solution'
+    description.appendChild(document.createElement('p')).textContent = `${project.solution}`
+
+    const imgContainer = section.appendChild(document.createElement('div'))
+    imgContainer.classList.add('imgContainer')
+    project.imgs.forEach((img) => {
+      imgContainer.appendChild(document.createElement('img')).src = img
+    })
   }
 }
 
